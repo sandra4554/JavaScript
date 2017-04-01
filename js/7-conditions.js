6d0ed5a459f74f422d8c0a9659d8de2de3d76b39
Sandra <ms.sandragilon@gmail.com>
2017-04-01 11:58:18 +0200

    warunki

---
+++ js/7-conditions.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var wzrostMateusza = 190;
+var wzrostOlgi = 160;
+
+/*
+	Warunek if
+
+*/
+
+if (wzrostOlgi < wzrostMateusza) {
+	console.log("Olga jest niższa :)");
+} 
+
+/*
+	Warunek if else
+
+*/
+
+if (wzrostOlgi > wzrostMateusza) {
+	console.log("Olga jest niższa :)");
+} else {
+	console.log("Olga jest wyższa");
+}
+
+/*
+	Warunek if else if
+
+*/
+
+if (wzrostOlgi > wzrostMateusza) {
+	console.log("Olga jest wyższa :)");
+} else if (wzrostOlgi == wzrostMateusza) {
+	console.log("Olga jest tak wysoka jak Mateusz");
+} else {
+	console.log("Olga jest niższa");
+}
+
+/*
+	Warunek switch
+
+*/
+var kolor = 'zielony';
+
+switch (kolor) {
+	case 'czerwony'
+		console.log('Kolor czerwony')
+		break;
+	case 'zielony'
+		console.log('Kolor zielony')
+		break;
+	case 'niebieski'
+		console.log('Kolor niebieski')
+		break;
+	default
+		console.log('Inny kolor')
+}
+
