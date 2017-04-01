fe2fc0a23202d6e9bc3465ed3e6b278ea3aa297f
Sandra <ms.sandragilon@gmail.com>
2017-04-01 09:39:30 +0200

    Zmienne

---
+++ js/2-variables.js
@@ -0,0 +1,13 @@
+'use strict';
+var imie;
+var imieDamskie = "Kaja";
+var wiek = 37;
+var kolor = "czerwony";
+
+console.log(wiek);
+console.log(kolor);
+console.log(imie);
+
+wiek = 21;
+
+console.log(wiek);
