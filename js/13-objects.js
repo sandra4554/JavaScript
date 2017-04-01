523d546d9edb933369ca884f994b8db9ce1e0d26
Sandra <ms.sandragilon@gmail.com>
2017-04-01 16:54:27 +0200

    obiekty

---
+++ js/13-objects.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var kaja = {
+	imie: "Kaja",
+	wzrost: 152,
+	przedstawOsobe: function() {console.log(this.imie)}
+}
+
+var krystian = {
+	imie: "Krystian",
+	wzrost: 152,
+	przedstawOsobe: function() {console.log(this.imie)}
+}
+
+kaja.przedstawOsobe();
+krystian.przedstawOsobe();
+
+console.log(kaja.wzrost); //odwołujemy się do danej cechy
+
+console.log(kaja); //cały obiekt
+
+function Osoba(imie, nazwisko) { //niepowtarzanmy kazdej funkcji tylko  opisujemy jedna dla wielu
+	
+	this.imie = imie;
+	this.nazwisko = nazwisko;
+	this.wzrost;
+	this.oczy;
+	this.wyswietlInfo = function() {
+		console.log("Imię: " + this.imie + "\n" 
+					+ "Nazwisko: " + this.nazwisko + "\n"
+				    + "Wzrost: " + this.wzrost + "\n"
+				   	+ "Oczy: " + this.oczy);
+	}
+}
+
+var krystian = new Osoba('Krystian', 'Dziopa');
+
+krystian.wzrost = 180;
+krystian.oczy = "niebieskie";
+
+krystian.wyswietlInfo();
+
+var kaja = new Osoba('Kaja', 'Stefaniak');
+kaja.wyswietlInfo();
