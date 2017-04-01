eb81b223ff23fffb47a1784540a4e475495af789
Sandra <ms.sandragilon@gmail.com>
2017-04-01 10:41:33 +0200

    typy danych

--- js/3-datatapes.js
+++ js/3-datatapes.js
@@ -12,3 +12,46 @@ var calkowitaWyplata;
 
 calkowitaWyplata = wyplata + premia;
 console.log(calkowitaWyplata);
+
+/*
+
+Typ-lańcuch znakow 
+
+*/
+
+var wyplataStr = "5000";
+var premiaStr = "2000";
+var calkowitaWyplataStr;
+
+calkowitaWyplataStr = wyplataStr + premiaStr;
+console.log(calkowitaWyplataStr);
+
+/*
+
+Typ logiczny- boolean
+
+*/
+
+var czyJestSmog;
+czyJestSmog = false;
+
+if (czyJestSmog){
+	console.log("Jest SMOG");
+}else {
+	console.log("Nie ma SMOGa ;)");
+}
+
+/*
+
+Typ specjalny- undefined / null
+
+*/
+
+var nieZdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log (nieZdefiniowanaZmienna);
+console.log (nullowaZmienna);
+
+var imieStudenta = "Michał";
+console.log(imieStudenta.indexOf('c'));
